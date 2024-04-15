709c5131235591ea47cab4a08ab4d46a10041724
iliadeleon <iliadeleon@gmail.com>
2024-04-15 18:05:00 -0400

    update background

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import BG from '@/public/RMCBG.png';
 import Navbar from '@/app/components/Navbar';
 import InfoSection from '@/app/components/InfoSection';
 import Resources from '@/app/components/Resources';
@@ -7,13 +5,6 @@ import Resources from '@/app/components/Resources';
 export default function Home() {
   return (
     <main className='relative min-h-fill w-full'>
-      <Image
-        src={BG}
-        alt='background-image'
-        fill
-        priority
-        className='absolute w-full h-full object-cover -z-10'
-      />
       <div className='relative z-1 text-[#1C1C1C] flex flex-col justify-between w-full py-10 px-5 md:px-40 space-y-4'>
         <Navbar />
         <InfoSection />
