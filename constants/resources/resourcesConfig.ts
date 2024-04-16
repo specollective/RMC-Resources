b8b3b42ce3e77a117e1f25e477d12ee9bc1fe7fe
Yoline <92892101+YolineB@users.noreply.github.com>
2024-04-15 18:43:01 -0700

    Reorganize and rename constants files

---
+++ constants/resources/resourcesConfig.ts
@@ -0,0 +1,8 @@
+export const resourceCategories = [
+  { id: 'covid-19', name: 'COVID-19' },
+  { id: 'digital-resilience', name: 'Housing Stability' },
+  { id: 'financial-stability', name: 'Financial Stability' },
+  { id: 'health-wellness', name: 'Health & Welness' },
+  { id: 'youth-and-education', name: 'Youth & Education' },
+  // { id: 'digital-resilience', name: 'Digital Resilience' },
+];
