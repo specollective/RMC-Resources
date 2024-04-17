511852f87ea97c94db54ff3ed0e98c22946372fc
Yoline <92892101+YolineB@users.noreply.github.com>
2024-04-17 16:00:22 -0700

    Add resident services data (#10)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import InfoSection from '@/app/components/InfoSection';
 import Resources from '@/app/components/Resources';
+// import ContactResidentServices from '@/app/components/ContactResidentServices';
 
 export default function Home() {
   return (
     <main className='space-y-20'>
       <InfoSection />
       <Resources />
+      {/* <ContactResidentServices /> */}
     </main>
   );
 }
