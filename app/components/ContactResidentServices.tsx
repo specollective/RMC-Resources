09f4cd64800ef7cb45b571cd664529311dc9d3b9
Yoline <92892101+YolineB@users.noreply.github.com>
2024-04-17 15:52:24 -0700

    Add resdient services data

---
+++ app/components/ContactResidentServices.tsx
@@ -0,0 +1,6 @@
+// import contactServices from '@/constants/contact-services.json';
+// import Card from '@/app/components/Card';
+
+export default function ContactResidentServices() {
+  return <section id='Contact-Resident-Services'></section>;
+}
