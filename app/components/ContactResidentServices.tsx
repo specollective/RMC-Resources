e167e13e73fea6e8b607b7bf4f4ee2bf00d9e072
iliadeleon <88496806+iliadeleon@users.noreply.github.com>
2024-04-18 12:22:22 -0400

    add contact box grid, update info, styling (#11)

--- app/components/ContactResidentServices.tsx
+++ app/components/ContactResidentServices.tsx
@@ -1,6 +1,32 @@
-// import contactServices from '@/constants/contact-services.json';
-// import Card from '@/app/components/Card';
+import contactServices from '@/constants/contact-services.json';
+import Card from '@/app/components/Card';
 
 export default function ContactResidentServices() {
-  return <section id='Contact-Resident-Services'></section>;
+  return (
+    <section id='Contact-Resident-Services'>
+      <h1 className='text-white'>CONTACT RESIDENT SERVICES</h1>
+      <Card className='grid grid-cols-3 gap-8 p-10'>
+        {contactServices.map((contact) => (
+          <div
+            key={contact.name}
+            className='justify-self-center text-center leading-loose'
+          >
+            <h2 className='font-semibold'>{contact.name}</h2>
+            <p>{contact.title}</p>
+            <p>{contact.number}</p>
+            <a
+              href={`mailto:${contact.email}`}
+              className='text-blue-500 underline'
+            >
+              {contact.email}
+            </a>
+          </div>
+        ))}
+        <div className='col-span-3 text-center'>
+          <p className='font-semibold'>Resident Services Drop-box</p>
+          <p className=''>Located on All Properties</p>
+        </div>
+      </Card>
+    </section>
+  );
 }
