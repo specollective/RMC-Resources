53ce8fe2c7c5988805ae0b8655e47cef2952e06c
iliadeleon <iliadeleon@gmail.com>
2024-04-18 12:10:54 -0400

    add contact box grid, update info, styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import InfoSection from '@/app/components/InfoSection';
 import Resources from '@/app/components/Resources';
-// import ContactResidentServices from '@/app/components/ContactResidentServices';
+import ContactResidentServices from '@/app/components/ContactResidentServices';
 
 export default function Home() {
   return (
     <main className='space-y-20'>
       <InfoSection />
       <Resources />
-      {/* <ContactResidentServices /> */}
+      {<ContactResidentServices />}
     </main>
   );
 }
