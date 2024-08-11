53f25d947ef20be459a287cbc69eb05ab8facf3a
Caitlin <cbuenlucas@gmail.com>
2024-08-11 10:47:45 -1000

    added CommunitiesGrid (#46)
    * added CommunitiesGrid
    
    * ran linter

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import InfoSection from '@/app/components/InfoSection';
-import Resources from '@/app/components/Resources';
+import CommunitiesGrid from '@/app/components/CommunitiesGrid';
 import ContactResidentServices from '@/app/components/ContactResidentServices';
 
 export default function Home() {
   return (
-    <main className='space-y-20'>
-      <InfoSection />
-      <Resources />
-      {<ContactResidentServices />}
+    <main className='space-y-6'>
+      <h1 className='font-bold text-[#FFB47F] text-xl tracking-wide leading-9'>
+        WELCOME TO RESIDENTS RESOURCE CENTER
+      </h1>
+      <CommunitiesGrid />
+      <ContactResidentServices />
     </main>
   );
 }
