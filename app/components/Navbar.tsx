2aeb07bbb1b0bbc49813cc9cdf73da70196b8055
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2024-08-10 19:26:20 -0700

    logo + language selection now showing for mobile devices, abbreviated lang for smaller screen size

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -6,10 +8,33 @@ import RMCLogoWhite from '@/public/RMCLogoWhite.png';
 const styles = {
   container: 'flex justify-between items-center text-white text-lg ',
   logo: 'flex flex-col gap-2 justify-self-center md:items-center font-semibold hover:underline ',
-  languageContainer: 'hidden md:block',
+  languageContainer: 'ml-[15px] md:block',
 };
 
 const Navbar = () => {
+  const getInitialScreenWidth = () => {
+    if (typeof window !== 'undefined') {
+      return window.matchMedia('(min-width: 768px)').matches ? false : true;
+    }
+    return true;
+  };
+
+  const [abbreviateLang, setAbbreviateLang] = useState(getInitialScreenWidth);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+    const handleAbbreviateLang = () => {
+      setAbbreviateLang(!mediaQuery.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleAbbreviateLang);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleAbbreviateLang);
+    };
+  }, []);
+
   return (
     <nav className={styles.container}>
       <Link href='/' className={styles.logo}>
@@ -17,11 +42,11 @@ const Navbar = () => {
       </Link>
       <div className={styles.languageContainer}>
         <Link className='mr-3' href='#' locale='en'>
-          English
+          {abbreviateLang ? 'EN' : 'English'}
         </Link>
         |
         <Link className='ml-3' href='#' locale='es'>
-          Spanish
+          {abbreviateLang ? 'ESP' : 'Spanish'}
         </Link>
       </div>
     </nav>
