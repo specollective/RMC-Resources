cde5bceedb1d54dc367264f19ad58d64e5e75060
Caitlin <cbuenlucas@gmail.com>
2024-08-11 10:07:46 -1000

    main announcement on home page for mobile now matches figma (#44)
    * main announcement on home page for mobile now matches figma
    
    * ran linter

--- app/components/FeatureContent.tsx
+++ app/components/FeatureContent.tsx
@@ -1,16 +1,40 @@
+'use client';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 import { FeatureHighlightContent } from '@/content/infoSection';
 
 const styles = {
   section: 'flex flex-row gap-4',
-  imageContainer: 'basis-1/2 h-full',
-  contentContainer: 'basis-1/2 p-10',
-  title: 'text-xl font-bold py-4',
+  imageContainer: 'md:basis-1/2 h-full hidden md:block object-cover',
+  contentContainer:
+    'md:flex items-center justify-center md:block md:basis-1/2 p-10',
+  title: 'text-xl font-bold md:text-2xl md:text-center',
   description: '',
 };
 
 export default function FeatureHighlight() {
+  const [title, setTitle] = useState(FeatureHighlightContent.featureTitle);
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    const updateDescription = () => {
+      if (window.innerWidth < 768) {
+        setTitle(FeatureHighlightContent.mobileFeatureTitle);
+        setDescription(FeatureHighlightContent.mobileDescription);
+      } else {
+        setTitle(FeatureHighlightContent.featureTitle);
+        setDescription('');
+      }
+    };
+
+    updateDescription();
+
+    window.addEventListener('resize', updateDescription);
+
+    return () => window.removeEventListener('resize', updateDescription);
+  }, []);
+
   return (
     <section className={styles.section}>
       <Image
@@ -21,10 +45,8 @@ export default function FeatureHighlight() {
         className={styles.imageContainer}
       />
       <div className={styles.contentContainer}>
-        <h2 className={styles.title}>{FeatureHighlightContent.featureTitle}</h2>
-        <p className={styles.description}>
-          {FeatureHighlightContent.description}
-        </p>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{description}</p>
       </div>
     </section>
   );
--- content/infoSection.ts
+++ content/infoSection.ts
@@ -1,9 +1,11 @@
 export const FeatureHighlightContent = {
   imagePath: '/placeholder.png',
   imageAlt: 'Computer Lab Now Open',
-  featureTitle: 'Computer Lab NOW OPEN!',
-  description:
-    'Join us in the computer lab, in the Community Building Room 2. Check for hours.',
+  // description:
+  //   'Join us in the computer lab, in the Community Building Room 2. Check for hours.',
+  featureTitle: 'Arroyo Village’s Computer Lab NOW OPEN!',
+  mobileFeatureTitle: 'Computer Lab NOW OPEN!',
+  mobileDescription: 'Arroyo Village’s computer lab is now open. Wednesdays/Thursdays 5:30pm-7:30pm'
 };
 
 export const scheduleItems = [
